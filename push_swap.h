647641521706e1b8e76e05cc7010054a95c31415
Anjara Manoela Andriamisaina <anjaraan@c1r9s14.42antananarivo.mg>
2026-03-06 15:53:25 +0300

    backup

---
+++ push_swap.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anjaraan <anjaraan@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 15:47:56 by anjaraan          #+#    #+#             */
+/*   Updated: 2026/03/06 15:48:50 by anjaraan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+int		is_sorted(t_stack *stack);
+void	simple_strategy(t_stack *a, t_stack *b);
+t_stack	*create_stack(void);
+void	push(t_stack *stack, int value);
+int		pop(t_stack *stack);
+void	free_stack(t_stack *stack);
+
+#endif
---
+++ simple_strategy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple_strategy.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anjaraan <anjaraan@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 15:46:55 by anjaraan          #+#    #+#             */
+/*   Updated: 2026/03/06 15:47:45 by anjaraan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	simple_strategy(t_stack *a, t_stack *b)
+{
+	if (is_sorted(a))
+		return ;
+	if (a->size == 2)
+	{
+		if (a->top->value > a->top->next->value)
+			sa(a);
+		return ;
+	}
+	if (a->size == 3)
+	{
+		if (a->top->value > a->top->next->value)
+			sa(a);
+		if (a->top->next->value > a->top->next->next->value)
+			rra(a);
+		if (a->top->value > a->top->next->value)
+			sa(a);
+		return ;
+	}
+	while (a->size > 3)
+		pb(a, b);
+	simple_strategy(a, b);
+	while (b->size > 0)
+		pa(a, b);
+}
